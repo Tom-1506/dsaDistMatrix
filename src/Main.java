fcaf2a707e661fce51b0a6b6914a1a9f29d74bf8
Tom <1599tm@gmail.com>
2018-11-20 11:38:29 +0000

    Decided I prefer using seperate line bracing still

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
-public class Main {
-    public static void findShortest(int[][] arrayIn){
-        for(int i = 0; i < arrayIn.length; i++){
+public class Main
+{
+    public static void findShortest(int[][] arrayIn)
+    {
+        for(int i = 0; i < arrayIn.length; i++)
+        {
             int smallest = arrayIn[i][0];
 
-            if(smallest == 0){
+            if(smallest == 0)
+            {
                 smallest = arrayIn[i][1];
             }
 
-            for(int j = 0; j < arrayIn[i].length; j++){
-                if(arrayIn[i][j] < smallest && arrayIn[i][j] != 0){
+            for(int j = 0; j < arrayIn[i].length; j++)
+            {
+                if(arrayIn[i][j] < smallest && arrayIn[i][j] != 0)
+                {
                     smallest = arrayIn[i][j];
                 }
             }
@@ -16,7 +22,8 @@ public class Main {
         }
     }
 
-    public static int[][] generateDistArray(){
+    public static int[][] generateDistArray()
+    {
         int[][] arrayOut =
                 {{0, 58, 184, 271, 378, 379},
                  {58, 0, 167, 199, 351, 382},
@@ -27,7 +34,8 @@ public class Main {
         return arrayOut;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         findShortest(generateDistArray());
     }
 }
