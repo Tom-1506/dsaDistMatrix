57492c2b728f253832d05b26c05ea594d506ff6c
Tom <1599tm@gmail.com>
2018-11-20 11:05:33 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void findShortest(int[][] arrayIn){
+
+
+    }
+
+    public static int[][] generateDistArray(int dimension){
+
+        int[][] arrayOut = new int[dimension][dimension];
+    }
+
+    public static void main(String[] args){
+
+
+    }
+}
