15288a2cab1b3121f5c3efcdd27630b8ebb886eb
Tom <1599tm@gmail.com>
2018-11-20 11:36:38 +0000

    Fixed my weird spacing

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
-import java.util.Random;
-
 public class Main {
-
     public static void findShortest(int[][] arrayIn){
-
         for(int i = 0; i < arrayIn.length; i++){
-
             int smallest = arrayIn[i][0];
+
             if(smallest == 0){
                 smallest = arrayIn[i][1];
             }
 
             for(int j = 0; j < arrayIn[i].length; j++){
-
                 if(arrayIn[i][j] < smallest && arrayIn[i][j] != 0){
                     smallest = arrayIn[i][j];
                 }
@@ -22,7 +17,6 @@ public class Main {
     }
 
     public static int[][] generateDistArray(){
-
         int[][] arrayOut =
                 {{0, 58, 184, 271, 378, 379},
                  {58, 0, 167, 199, 351, 382},
@@ -34,7 +28,6 @@ public class Main {
     }
 
     public static void main(String[] args){
-
         findShortest(generateDistArray());
     }
 }
