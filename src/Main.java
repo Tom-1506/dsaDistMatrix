346e9e18a748129a3e16686bc954b4305897b94f
Tom <1599tm@gmail.com>
2018-11-27 12:11:34 +0000

    added the "optimised" version of the find shortest method

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,38 @@ public class Main{
         return shortestArray;
     }
 
+    public static int[] findShortestOp(int[][] arrayIn){
+        int[] shortestArray = new int[arrayIn.length];
+
+        for(int i = 0; i < arrayIn.length; i++){
+            int shortest = arrayIn[i][0];
+
+            if(shortest == 0){
+                shortest = arrayIn[i + 1][0];
+                for (int j = 0; j < arrayIn[i].length; j++){
+                    if(arrayIn[j][i] < shortest && arrayIn[j][i] != 0){
+                        shortest = arrayIn[j][i];
+                    }
+                }
+            }
+
+            for(int j = 0; j < arrayIn[i].length; j++){
+                if(arrayIn[i][j] < shortest && arrayIn[i][j] != 0){
+                    shortest = arrayIn[i][j];
+                }
+                if(arrayIn[i][j] == 0){
+                    for (int k = 0; k < arrayIn.length; k++){
+                        if(arrayIn[k][i] < shortest && arrayIn[k][i] != 0){
+                            shortest = arrayIn[k][i];
+                        }
+                    }
+                }
+            }
+            shortestArray[i] = shortest;
+        }
+        return shortestArray;
+    }
+
     public static int[][] generateDistArray(int dimension){
         int[][] arrayOut = new int[dimension][dimension];
         Random randNum = new Random();
@@ -66,6 +98,6 @@ public class Main{
     }
 
     public static void main(String[] args){
-        timeMatrix(100, 100, 10, 100);
+        timeMatrix(200, 100, 10, 100);
     }
 }
